2d667ace5e04f1fe1040433f986a77458b742bf7
Renato Del Gaudio <renato.delgaudio@gmail.com>
2024-12-16 21:06:17 +0100

    fix env setup

--- env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export CHAIN_ID=namada.5f5de2dd1b88cba30586420
-export NAMADA_NETWORK_CONFIGS_SERVER="https://github.com/anoma/namada-mainnet-genesis/releases/download/mainnet-genesis"
--- main.sh
+++ main.sh
@@ -2,8 +2,9 @@
 
 echo Running bash script in github
 
-./env.sh 
-env
+export CHAIN_ID=namada.5f5de2dd1b88cba30586420
+export NAMADA_NETWORK_CONFIGS_SERVER="https://github.com/anoma/namada-mainnet-genesis/releases/download/mainnet-genesis"
+
 
 namada --version
 
