13034a2223deb2bd927ccca30d5ee79e9fe1271c
Renato Del Gaudio <renato.delgaudio@gmail.com>
2024-12-26 10:58:37 +0100

    Updated with the new claimed NAM

--- main.sh
+++ main.sh
@@ -11,8 +11,8 @@ echo Running bash script in github
 export VALIDATOR_ADDRESS=tnam1qyvc8n7ufmpan4yy3nemm3td2c6uff4m6s72g3eg
 
 # Update it when commission are claimed by the validator
-CLAIMED=107.1353
-TOTAL_STAKE_AVERAGE=667500
+CLAIMED=193.4379
+TOTAL_STAKE_AVERAGE=1168629
 #16.12.2024 23.00
 TOTAL_START_TIME=1734390000
 
