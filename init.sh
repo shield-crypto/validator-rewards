67e341b552a5440f071cc9c3abc96e2906a7ac6b
Renato Del Gaudio <renato.delgaudio@gmail.com>
2025-01-10 21:22:34 +0100

    fix

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ echo Running bash script in github
 
 start_time=$(date +%s)
 LAST_RUN_FILE=metrics/last_run.txt
-LAST_REWARDS_FILE=last_rewards_delegators.txt
+LAST_REWARDS_FILE=metrics/last_rewards_delegators.txt
 
 echo $start_time > $LAST_RUN_FILE
 echo 0 > $LAST_REWARDS_FILE
