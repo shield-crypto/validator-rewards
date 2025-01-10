06de7bad45dffd5d6e831a71a491c99056339039
Renato Del Gaudio <renato.delgaudio@gmail.com>
2025-01-10 21:17:53 +0100

    reinit

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo Running bash script in github
+
+start_time=$(date +%s)
+LAST_RUN_FILE=metrics/last_run.txt
+LAST_REWARDS_FILE=last_rewards_delegators.txt
+
+echo $start_time > $LAST_RUN_FILE
+echo 0 > $LAST_REWARDS_FILE
