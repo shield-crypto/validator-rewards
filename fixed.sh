5fc8852f159814fc330e72e609b568316e6e8e74
Renato Del Gaudio <renato.delgaudio@gmail.com>
2025-06-25 22:18:36 +0200

    fix website

--- fixed.sh
+++ fixed.sh
@@ -4,8 +4,11 @@ echo Running bash script in github
 
 
 METRIC_FILE=metrics/metrics.txt
+WEBSITE_APY=metrics/last_delegator_apy.txt
 
-echo "shield_delegators_apy .13" >> $METRIC_FILE
+echo "shield_delegators_apy .15" >> $METRIC_FILE
+echo ".15" >> $WEBSITE_APY
+
 
 
 
