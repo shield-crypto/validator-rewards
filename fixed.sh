a5ac7e34b4d9143431fa454301f88e42340befc9
Renato Del Gaudio <renato.delgaudio@gmail.com>
2025-06-25 22:20:56 +0200

    fix site

--- fixed.sh
+++ fixed.sh
@@ -9,10 +9,6 @@ WEBSITE_APY=metrics/last_delegator_apy.txt
 echo "shield_delegators_apy .15" >> $METRIC_FILE
 echo ".15" >> $WEBSITE_APY
 
-<<<<<<< HEAD
-=======
-echo "shield_delegators_apy 13" >> $METRIC_FILE
->>>>>>> 6950b8f16c2a72a222809e9a15e114544213fe7b
 
 
 
