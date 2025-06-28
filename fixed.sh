ce1b9dc97da8ea1887be60fd3fe63d693ab1e486
Renato Del Gaudio <renato.delgaudio@gmail.com>
2025-06-28 08:48:05 +0200

    metric fix

--- fixed.sh
+++ fixed.sh
@@ -6,6 +6,8 @@ echo Running bash script in github
 METRIC_FILE=metrics/metrics.txt
 WEBSITE_APY=metrics/last_delegator_apy.txt
 
+echo "# HELP shield_delegators_apy APY generated for delegators by the validator" >> $METRIC_FILE
+echo "# TYPE shield_delegators_apy gauge" >> $METRIC_FILE
 echo "shield_delegators_apy .15" >> $METRIC_FILE
 echo ".15" >> $WEBSITE_APY
 
