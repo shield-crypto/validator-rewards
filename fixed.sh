d0037fd6c6a2559829984b103cfaaf490c6e1253
Renato Del Gaudio <renato.delgaudio@gmail.com>
2025-07-10 21:24:41 +0200

    Update fixed.sh
    APY update

--- fixed.sh
+++ fixed.sh
@@ -8,8 +8,8 @@ WEBSITE_APY=metrics/last_delegator_apy.txt
 
 echo "# HELP shield_delegators_apy APY generated for delegators by the validator" >> $METRIC_FILE
 echo "# TYPE shield_delegators_apy gauge" >> $METRIC_FILE
-echo "shield_delegators_apy .15" >> $METRIC_FILE
-echo ".15" >> $WEBSITE_APY
+echo "shield_delegators_apy .14" >> $METRIC_FILE
+echo ".14" >> $WEBSITE_APY
 
 
 
